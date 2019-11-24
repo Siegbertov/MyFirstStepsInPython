b2dd0678a9c6472ab0a98022e8baa88dbd48fb09
Vladyslav Ziberov <siegbertov@gmail.com>
2019-11-24 19:43:08 +0200

    Printed PI

--- first.py
+++ first.py
@@ -1,4 +1,7 @@
+import math
 print("Hello, Github World!")
 print("This is texted from pyCharm")
 print("This is my next line added in a 5 min")
 print("All those line were added by pyCharm")
+
+print(math.pi)
